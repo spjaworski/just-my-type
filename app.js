bcd154014448d2b1e516a65d79f0017294881366
Scott Jaworski <scottpjaworski@gmail.com>
2021-11-01 20:22:38 -0400

    Some highlighting done, need to fix shift

--- app.js
+++ app.js
@@ -4,10 +4,83 @@ $(document).ready(function(){
 
     $("#keyboard-upper-container").hide();
 
-    $("#keyboard-upper-container").keydown(function(e){
-        if(e.which == 17) {
-            console.log(shift);
+    $(document).on("keydown", upperShift);
+    
+    if (event.which = 16) {
+    function upperShift () {
+         
             $("#keyboard-upper-container").show();
+            $("#keyboard-lower-container").hide();
+            
+        
+    }}
+
+    $(document).on("keyup", lowerShift);
+
+    if(event.which = 16) {
+    function lowerShift() {
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+            
+    }}
+    
+
+    $(document).keydown(function(){
+        console.log(event.which);
+        console.log(event.key);
+        console.log(event.keyCode);
+        if (event.keyCode == 65) {
+            $('#97').css("background-color", "yellow")
+        } else if (event.keyCode == 83) {
+            $('#115').css("background-color", "yellow")
+        } else if (event.keyCode == 68) {
+            $('#100').css("background-color", "yellow")
+        } else if (event.keyCode == 70) {
+            $('#102').css("background-color", "yellow")
+        } else if (event.keyCode == 71) {
+            $('#103').css("background-color", "yellow")
+        } else if (event.keyCode == 72) {
+            $('#104').css("background-color", "yellow")
+        } else if (event.keyCode == 74) {
+            $('#106').css("background-color", "yellow")
+        } else if (event.keyCode == 75) {
+            $('#107').css("background-color", "yellow")
+        } else if (event.keyCode == 76) {
+            $('#108').css("background-color", "yellow")
+        } else if (event.keyCode == 59) {
+            $('#59').css("background-color", "yellow")
+        } else if (event.keyCode == 222) {
+            $('#39').css("background-color", "yellow")
         }
 })
-})
+
+    $(document).keyup(function() {
+        console.log("released");
+        console.log(event.keyCode)
+        if (event.keyCode == 65) {
+            $('#97').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 83) {
+            $('#115').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 68) {
+            $('#100').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 70) {
+            $('#102').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 71) {
+            $('#103').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 72) {
+            $('#104').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 74) {
+            $('#106').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 75) {
+            $('#107').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 76) {
+            $('#108').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 59) {
+            $('#59').css("background-color", "#f5f5f5")
+        } else if (event.keyCode == 222) {
+            $('#39').css("background-color", "#f5f5f5")
+        }
+    })
+
+
+}) 
